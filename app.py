4b38ad60955f17eab7db2ea192adf78f6e7bde48
Mihir Pande <mihir.pande22@pccoepune.org>
2023-08-03 22:53:40 +0530

    fixing bugs

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ import ssl, smtplib
 from dotenv import load_dotenv
 import os
 
-
 load_dotenv()
-
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -22,7 +20,16 @@ email_sender = os.getenv('EMAIL_SENDER')
 gmail_password = os.getenv('EMAIL_PASSWORD')
 
 # Initialize Firebase config
-firebase_config = os.getenv('FIREBASE_CONFIG')
+firebase_config = {
+  'apiKey': os.getenv('API_KEY'),
+  'authDomain': os.getenv('AUTH_DOMAIN'),
+  'databaseURL' : os.getenv('DATABASE_URL'),
+  'projectId': os.getenv('PROJECT_ID'),
+  'storageBucket': os.getenv('STORAGE_BUCKET'),
+  'messagingSenderId': os.getenv('MESSAGING_SENDER_ID'),
+  'appId': os.getenv('APP_ID'),
+  'measurementId': os.getenv('MEASUREMENT_ID')
+}
 
 firebase = pyrebase.initialize_app(firebase_config)
 auth = firebase.auth()
